41a2db17567d8c058abf625b3795e8e61970ef73
Alex LaFroscia <alexlafroscia@nestlabs.com>
2016-12-13 17:10:15 -0800

    Add addon logic

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-/* jshint node: true */
 'use strict';
 
+const debug = require('debug')('ember-cli-polyfill-io:addon');
+const buildSrc = require('./lib');
+
 module.exports = {
-  name: 'ember-cli-polyfill-io'
+  name: 'polyfill-io',
+
+  isDevelopingAddon() {
+    return true;
+  },
+
+  contentFor(type, config) {
+    if (type !== 'head') {
+      return;
+    }
+
+    const polyfillConfig = config['polyfill-io'] || {};
+    const src = buildSrc(polyfillConfig);
+
+    debug(`Using script src: ${src}`);
+
+    return `
+      <script src="${src}"></script>
+    `;
+  }
 };
